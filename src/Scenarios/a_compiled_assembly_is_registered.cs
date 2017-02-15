b747d50512063db3274d5fa81db9fc38b27b5d65
Andy Alm <andy.alm@gettyimages.com>
2017-02-14 16:53:03 -0800

    Downgraded NUnit to a version more compatible with Kekiri

--- src/Scenarios/a_compiled_assembly_is_registered.cs
+++ src/Scenarios/a_compiled_assembly_is_registered.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Kekiri;
+using NUnit.Framework;
 using XRepo.Scenarios.Steps;
 using XRepo.Scenarios.TestSupport;
 
 namespace XRepo.Scenarios
 {
-    public class a_compiled_assembly_is_registered : FluentTest<XRepoEnvironmentContext>
+    [TestFixture]
+    public class HelloWorldFixture
     {
-        public a_compiled_assembly_is_registered()
+        [Test]
+        public void HelloWorld() { }
+    }
+
+    public class A_compiled_assembly_is_registered : FluentTest<XRepoEnvironmentContext>
+    {
+        public A_compiled_assembly_is_registered()
         {
             Given<a_class_library_project>();
             When<the_project_is_compiled>();
@@ -24,12 +32,12 @@ namespace XRepo.Scenarios
         public a_pinned_assembly_overrides_hint_paths_by_default()
         {
             Given<a_class_library_project>()
-                .And<the_project_has_a_reference_to_assembly_ASSEMBLYNAME>("XRepo.Scenarios")
-                .And<the_assembly_ASSEMBLYNAME_is_registered>("XRepo.Scenarios")
-                .And<the_assembly_ASSEMBLYNAME_is_pinned>("XRepo.Scenarios")
+                .And<the_project_has_a_reference_to_assembly_ASSEMBLYNAME>("nunit.framework")
+                .And<the_assembly_ASSEMBLYNAME_is_registered>("nunit.framework")
+                .And<the_assembly_ASSEMBLYNAME_is_pinned>("nunit.framework")
                 .And<the_SETTINGNAME_config_setting_is_SETTINGVALUE>("copypins", "false");
             When<the_project_is_compiled>();
-            Then<the_reference_is_resolved_to_the_pinned_copy_of_ASSEMBLYNAME>("XRepo.Scenarios");
+            Then<the_reference_is_resolved_to_the_pinned_copy_of_ASSEMBLYNAME>("nunit.framework");
         }
     }
 
@@ -38,13 +46,13 @@ namespace XRepo.Scenarios
         public a_pinned_assembly_is_copied_to_hint_path_location_when_copypins_is_true()
         {
             Given<a_class_library_project>()
-                .And<the_project_has_a_reference_to_assembly_ASSEMBLYNAME>("XRepo.Scenarios")
-                .And<the_assembly_ASSEMBLYNAME_is_registered>("XRepo.Scenarios")
-                .And<the_assembly_ASSEMBLYNAME_is_pinned>("XRepo.Scenarios")
+                .And<the_project_has_a_reference_to_assembly_ASSEMBLYNAME>("nunit.framework")
+                .And<the_assembly_ASSEMBLYNAME_is_registered>("nunit.framework")
+                .And<the_assembly_ASSEMBLYNAME_is_pinned>("nunit.framework")
                 .And<the_SETTINGNAME_config_setting_is_SETTINGVALUE>("copypins", "true");
             When<the_project_is_compiled>();
-            Then<the_registered_copy_of_ASSEMBLYNAME_is_copied_to_the_hint_paths_location>("XRepo.Scenarios")
-                .And<a_backup_copy_of_the_original_ASSEMBLYNAME_is_kept>("XRepo.Scenarios");
+            Then<the_registered_copy_of_ASSEMBLYNAME_is_copied_to_the_hint_paths_location>("nunit.framework")
+                .And<a_backup_copy_of_the_original_ASSEMBLYNAME_is_kept>("nunit.framework");
         }
     }
 
@@ -53,11 +61,11 @@ namespace XRepo.Scenarios
         public An_unpinned_assembly_does_not_override_the_hint_path()
         {
             Given<a_class_library_project>()
-                .And<the_project_has_a_reference_to_assembly_ASSEMBLYNAME>("XRepo.Scenarios")
-                .And<the_assembly_ASSEMBLYNAME_is_registered>("XRepo.Scenarios")
-                .And<the_assembly_ASSEMBLYNAME_is_not_pinned>("XRepo.Scenarios");
+                .And<the_project_has_a_reference_to_assembly_ASSEMBLYNAME>("nunit.framework")
+                .And<the_assembly_ASSEMBLYNAME_is_registered>("nunit.framework")
+                .And<the_assembly_ASSEMBLYNAME_is_not_pinned>("nunit.framework");
             When<the_project_is_compiled>();
-            Then<the_reference_to_ASSEMBLYNAME_is_resolved_via_standard_msbuild_rules>("XRepo.Scenarios");
+            Then<the_reference_to_ASSEMBLYNAME_is_resolved_via_standard_msbuild_rules>("nunit.framework");
         }
     }
 
@@ -68,12 +76,12 @@ namespace XRepo.Scenarios
         {
             Given<a_repo_REPONAME>("MyRepo")
                 .And<a_class_library_project>()
-                .And<the_project_has_a_reference_to_assembly_ASSEMBLYNAME>("XRepo.Scenarios")
-                .And<the_assembly_ASSEMBLYNAME_is_registered_at_a_location_within_REPONAME>("XRepo.Scenarios", "MyRepo")
+                .And<the_project_has_a_reference_to_assembly_ASSEMBLYNAME>("nunit.framework")
+                .And<the_assembly_ASSEMBLYNAME_is_registered_at_a_location_within_REPONAME>("nunit.framework", "MyRepo")
                 .And<the_repo_REPONAME_is_pinned>("MyRepo")
                 .And<the_SETTINGNAME_config_setting_is_SETTINGVALUE>("copy_pins", "false");
             When<the_project_is_compiled>();
-            Then<the_reference_is_resolved_to_the_pinned_copy_of_ASSEMBLYNAME>("XRepo.Scenarios");
+            Then<the_reference_is_resolved_to_the_pinned_copy_of_ASSEMBLYNAME>("nunit.framework");
         }
     }
 
